78f3a957744546c2ffc9a792e3be8163cdb67796
Kevin Miller <Airswoop1@gmail.com>
2016-05-06 14:23:02 -0400

    implemented handful of branch.io web sdk methods

--- branch.io/index.d.ts
+++ branch.io/index.d.ts
@@ -8,6 +8,14 @@ interface branchOptions {}
 interface branch {
   init(branch_key: string, options: branchOptions, callback: (err: any, data: any) => void): void;
   data(callback: (err: any, data: any) => void): void;
+
+  link(data: any, callback: (err: any, data: any) => void):void;
+  sendSMS(phone: string, linkData: any, options: any, callback: (err: any, data: any) => void):void;
+  deepview(data: any, options: any, callback: (err: any, data: any) => void): void;
+  deepviewCta(): void;
+
+  banner(options: any, data: any): void;
+  closeBanner(): void;
 }
 
 declare module 'branch' {
